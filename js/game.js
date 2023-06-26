154532ec0fc7ae8aa7fc377cdaf9418b4843ac6f
tylerhand1 <tyha7462@colorado.edu>
2023-06-26 16:30:39 -0600

    Added functions to clear board, switch player, add event listeners and updated make move to allow for clicks to make move

--- js/game.js
+++ js/game.js
@@ -12,9 +12,15 @@ const playerTwo = Player('O', 0);
 
 const gameboard = (function () {
     let currPlayer = 0;
-    const players = ['X', 'Y'];
+    const players = ['X', 'O'];
     const board = [['', '', ''], ['', '', ''], ['', '', '']];
-    const _checkGameOver = () => {
+    const boardElements = [...document.querySelectorAll('.game-space')];
+
+    const createEventListeners = () => {
+        boardElements.forEach(boardElement => boardElement.addEventListener('click', makeMove));
+    };
+
+    const _checkGameOver = () => {s
         // Check rows
         for(let i = 0; i < board.length; i++) {
             if(board[i][0] === '' || board[i][1] === ''  || board[i][2] === '' ) { /* Check if any relevant space empty */
@@ -42,21 +48,68 @@ const gameboard = (function () {
         
         // Check bottom left-top right diagonal
         if(board[2][0] === '' && board[1][1] === ''  && board[0][2] === '' ) { /* Check if any relevant space empty */
-            if(board[2][0] === board[1][i] && board[1][i] === board[0][2]) {
+            if(board[2][0] === board[1][1] && board[1][1] === board[0][2]) {
                 return true;
             }
         }
         // No win found
         return false;
     };
-    const makeMove = (row, col, player) => {
+
+    const _updateBoard = () => {
+        let counter = 0;
+        for(let i = 0; i < board.length; i++) {
+            for(let j = 0; j < board[i].length; j++) {
+                boardElements[counter++].textContent = board[i][j];
+            }
+        }
+    };
+
+    const _switchPlayer = () => {
+        currPlayer = 1 - currPlayer;
+    }
+
+    const makeMove = (e) => {
+        // Convert data-index to (row, col)
+        let row, col;
+        if(e.target.attributes[1].value < 3) {
+            row = 0;
+            col = e.target.attributes[1].value;
+        } else if (e.target.attributes[1].value < 6) {
+            row = 1;
+            col = e.target.attributes[1].value - 3;
+        } else {
+            row = 2;
+            col = e.target.attributes[1].value - 6;
+        }
         if(board[row][col] !== '') {
             return;
         }
-        board[row][col] = player;
+        board[row][col] = players[currPlayer];
+        _updateBoard();
         _checkGameOver();
-    }
+        _switchPlayer();
+    };
+
+    const clearBoard = () => {
+        for(let i = 0; i < board.length; i++) {
+            for(let j = 0; j < board[i].length; j++) {
+                board[i][j] = '';
+            }
+        }
+    };
+
+    const _printBoard = () => {
+        for(let i = 0; i < board.length; i++) {
+            console.log(board[i][0], board[i][1], board[i][2]);
+        }
+    };
+
     return {
+        createEventListeners,
         makeMove,
+        clearBoard,
     };
 })();
+
+gameboard.createEventListeners();
